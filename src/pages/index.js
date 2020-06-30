56766a27a0e4ec99cd1129413fb537fd9124dd50
Henri Fardeau <henri.fardeau@outlook.fr>
2020-06-30 14:23:12 +0200

    feat: adding hello-world netlify functions

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const [hello, setHello] = useState("Hello...")
+
+  const fetchHello = async () => {
+    const helloWorldApi = await fetch(
+      "/.netlify/functions/hello-world?name=Grigor"
+    ).then(resp => resp.json())
+
+    setHello(helloWorldApi.message)
+  }
+
+  return (
+    <div>
+      <p>{hello}</p>
+      <button type="button" onClick={() => fetchHello()}>
+        Hello ?
+      </button>
+    </div>
+  )
 }
